940983baba1c26686ec424646be3edd22b2782bd
Ahmad Khoirudin Affandi <113109420+akhrdnaffandi@users.noreply.github.com>
2025-05-25 21:32:19 +0700

    Add files via upload
    menambahkan folder model, yang berisikan KaryawanModel.cs dan UserModel.cs

---
+++ Manajemen-Data-Karyawan/Model/KaryawanModel.cs
@@ -0,0 +1,87 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace Manajemen_Data_Karyawan.Model
+{
+    public class KaryawanModel
+    {
+        private MySqlConnection conn;
+
+        public KaryawanModel()
+        {
+            conn = new MySqlConnection("server=localhost;user=root;password=;database=karyawan_db;");
+        }
+
+        public DataTable LoadData()
+        {
+            DataTable dt = new DataTable();
+            string query = "SELECT * FROM karyawan";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        public void SimpanData(Karyawan karyawan)
+        {
+            string query = @"INSERT INTO karyawan (Nama, Jabatan, Gaji, TanggalBergabung, Status, Telepon, Alamat, Email) 
+                             VALUES (@Nama, @Jabatan, @Gaji, @TanggalBergabung, @Status, @Telepon, @Alamat, @Email)";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Nama", karyawan.Nama);
+            cmd.Parameters.AddWithValue("@Jabatan", karyawan.Jabatan);
+            cmd.Parameters.AddWithValue("@Gaji", karyawan.Gaji);
+            cmd.Parameters.AddWithValue("@TanggalBergabung", karyawan.TanggalBergabung);
+            cmd.Parameters.AddWithValue("@Status", karyawan.Status);
+            cmd.Parameters.AddWithValue("@Telepon", karyawan.Telepon);
+            cmd.Parameters.AddWithValue("@Alamat", karyawan.Alamat);
+            cmd.Parameters.AddWithValue("@Email", karyawan.Email);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void Edit(Karyawan karyawan)
+        {
+            string query = @"UPDATE karyawan SET Nama=@Nama, Jabatan=@Jabatan, Gaji=@Gaji, TanggalBergabung=@TanggalBergabung,
+                             Status=@Status, Telepon=@Telepon, Alamat=@Alamat, Email=@Email WHERE ID=@ID";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@ID", karyawan.ID);
+            cmd.Parameters.AddWithValue("@Nama", karyawan.Nama);
+            cmd.Parameters.AddWithValue("@Jabatan", karyawan.Jabatan);
+            cmd.Parameters.AddWithValue("@Gaji", karyawan.Gaji);
+            cmd.Parameters.AddWithValue("@TanggalBergabung", karyawan.TanggalBergabung);
+            cmd.Parameters.AddWithValue("@Status", karyawan.Status);
+            cmd.Parameters.AddWithValue("@Telepon", karyawan.Telepon);
+            cmd.Parameters.AddWithValue("@Alamat", karyawan.Alamat);
+            cmd.Parameters.AddWithValue("@Email", karyawan.Email);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void Hapus(int id)
+        {
+            string query = "DELETE FROM karyawan WHERE ID=@ID";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@ID", id);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public DataTable CariData(string keyword)
+        {
+            DataTable dt = new DataTable();
+            string query = "SELECT * FROM karyawan WHERE Nama LIKE @keyword OR Jabatan LIKE @keyword";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@keyword", "%" + keyword + "%");
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            adapter.Fill(dt);
+            return dt;
+        }
+    }
+}
---
+++ Manajemen-Data-Karyawan/Model/UserModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Manajemen_Data_Karyawan.Model
+{
+    public class UserModel
+    {
+        private MySqlConnection conn;
+
+        public UserModel()
+        {
+            conn = new MySqlConnection("server=localhost;user=root;password=;database=karyawan_db;");
+        }
+
+        public bool CekLogin(string username, string password)
+        {
+            string query = "SELECT * FROM users WHERE username=@username AND password=@password";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+
+            conn.Open();
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+
+            return count > 0;
+        }
+    }
+}

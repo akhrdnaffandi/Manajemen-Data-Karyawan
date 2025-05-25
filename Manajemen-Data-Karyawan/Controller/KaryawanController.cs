979f46ecb893c70e737360e9ab8e5a410430131b
Ahmad Khoirudin Affandi <113109420+akhrdnaffandi@users.noreply.github.com>
2025-05-25 21:30:53 +0700

    Add files via upload
    menambahkan folder controller

---
+++ Manajemen-Data-Karyawan/Controller/KaryawanController.cs
@@ -0,0 +1,40 @@
+﻿using Manajemen_Data_Karyawan.Model;
+using System.Data;
+
+namespace Manajemen_Data_Karyawan.Controller
+{
+    public class KaryawanController
+    {
+        private KaryawanModel model;
+
+        public KaryawanController()
+        {
+            model = new KaryawanModel();
+        }
+
+        public DataTable GetAllKaryawan()
+        {
+            return model.LoadData();
+        }
+
+        public void TambahKaryawan(Karyawan karyawan)
+        {
+            model.SimpanData(karyawan);
+        }
+
+        public void UpdateKaryawan(Karyawan karyawan)
+        {
+            model.Edit(karyawan);
+        }
+
+        public void DeleteKaryawan(int id)
+        {
+            model.Hapus(id);
+        }
+
+        public DataTable CariKaryawan(string keyword)
+        {
+            return model.CariData(keyword);
+        }
+    }
+}
---
+++ Manajemen-Data-Karyawan/Controller/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Manajemen_Data_Karyawan.Model;
+
+namespace Manajemen_Data_Karyawan.Controller
+{
+    public class LoginController
+    {
+        private UserModel userModel;
+
+        public LoginController()
+        {
+            userModel = new UserModel();
+        }
+
+        public bool Login(string username, string password)
+        {
+            return userModel.CekLogin(username, password);
+        }
+    }
+}
